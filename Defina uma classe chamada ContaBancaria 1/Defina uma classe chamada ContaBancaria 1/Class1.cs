2c4fe74bd9a2978042cb61e936c979f8a9403949
Rita Mara Santos <124637586+ritamarasantos@users.noreply.github.com>
2023-02-13 18:15:07 -0300

    Add files via upload
    M[M1S02] Ex 3 - Defina uma classe chamada ContaBancaria

---
+++ Defina uma classe chamada ContaBancaria 1/Defina uma classe chamada ContaBancaria 1/Class1.cs
@@ -0,0 +1,12 @@
+﻿
+//[M1S02] Ex 3 - Defina uma classe chamada ContaBancaria//
+
+namespace Classes;
+public class ContaBancaria
+{
+    public int numero;
+    public int agencia;
+    public string nomeTitular;
+    public decimal saldo = 0;
+    public TipoContaEnum tipoConta;
+}
