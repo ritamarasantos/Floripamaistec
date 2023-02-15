2221ec1ee14ac891406c0bd5a691340b31ca27c8
Rita Mara Santos <124637586+ritamarasantos@users.noreply.github.com>
2023-02-14 23:13:11 -0300

    Add files via upload
    [M1S03] Ex 1 - Criação de classes e construtor

---
+++ Criação_de_classes_e_construtores/Criação_de_classes_e_construtores/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criação_de_classes_e_construtores
+{
+    public class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public Produto()
+        {
+            nome = "sem nome";
+            preco= 0;   
+            quantidade= 0;
+        }
+    }
+
+
+}
+
---
+++ Criação_de_classes_e_construtores/Criação_de_classes_e_construtores/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using Criação_de_classes_e_construtores;
+
+Produto produto1 = new Produto("calculadora", 5.75, 199);
+Produto produto2 = new Produto();
+Console.WriteLine($"O produto1 tem nome: {produto1.nome}, valor: {produto1.preco}, quantidade {produto1.quantidade}.");
+Console.WriteLine($"O produto2 tem nome: {produto2.nome}, valor: {produto2.preco}, quantidade {produto2.quantidade}.");
